118d131085d07b878926272939eb327724655967
SEK7I0N <stewari280303@gmail.com>
2022-04-27 11:53:08 +0530

    database update

--- backend/db/database.py
+++ backend/db/database.py
@@ -14,7 +14,7 @@ def create_user_table(database_name):
     """Creates a user table"""
     database = sqlite3.connect(f'backend/db/{database_name}.db')
     cursor = database.cursor()
-    cursor.execute('''CREATE TABLE USERS
+    cursor.execute('''CREATE TABLE IF NOT EXISTS USERS
             (ID INT PRIMARY KEY     NOT NULL,
             NAME           TEXT    NOT NULL,
             AGE            INT     NOT NULL,
@@ -22,25 +22,35 @@ def create_user_table(database_name):
             EMAIL          CHAR(500),
             PASSWORD       CHAR(500),
             SALARY  INT);''')
+    print('Table USERS created')
+    cursor.close()
+    database.close()
 
 def create_role_table(database_name):
     """Creates a user table"""
     database = sqlite3.connect(f'backend/db/{database_name}.db')
     cursor = database.cursor()
-    cursor.execute('''CREATE TABLE ROLES
+    cursor.execute('''CREATE TABLE IF NOT EXISTS ROLES
             (ID INT PRIMARY KEY     NOT NULL,
             ROLE           TEXT    NOT NULL);''')
+    print('Table ROLES created')
+    cursor.close()
+    database.close()
+
 
 def create_user_role_table(database_name):
     """Creates a user table"""
     database = sqlite3.connect(f'backend/db/{database_name}.db')
     cursor = database.cursor()
-    cursor.execute('''CREATE TABLE USER_ROLES
+    cursor.execute('''CREATE TABLE IF NOT EXISTS USER_ROLES
             (ID INT PRIMARY KEY     NOT NULL,
             USER_ID           INT    NOT NULL ,
             ROLE_ID           INT    NOT NULL,
             FOREIGN KEY (USER_ID) REFERENCES USERS(ID),
             FOREIGN KEY (ROLE_ID) REFERENCES ROLES(ID));''')
+    print('Table USER_ROLES created')
+    cursor.close()
+    database.close()
 
 def main():
     """Main function"""
